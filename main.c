40cdd061da7ed0ce92108a50039e469b3a7e8cd3
mokrz <5460522+mokrz@users.noreply.github.com>
2020-12-15 08:29:08 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,29 @@
-// A C program to demonstrate buffer overflow 
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
-  
-int main(int argc, char *argv[]) 
-{ 
-       char buffer[5];
-       
-       if (argc < 2) 
-       { 
-              printf("strcpy() NOT executed....\n"); 
-              printf("Syntax: %s <characters>\n", argv[0]); 
-              exit(0); 
-       } 
-  
-       strcpy(buffer, argv[1]); 
-       printf("buffer content= %s\n", buffer); 
-       printf("strcpy() executed...\n"); 
-  
-       return 0; 
-} 
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char buff[15];
+    int pass = 0;
+
+    printf("\n Enter the password : \n");
+    gets(buff);
+
+    if(strcmp(buff, "thegeekstuff"))
+    {
+        printf ("\n Wrong Password \n");
+    }
+    else
+    {
+        printf ("\n Correct Password \n");
+        pass = 1;
+    }
+
+    if(pass)
+    {
+       /* Now Give root or admin rights to user*/
+        printf ("\n Root privileges given to the user \n");
+    }
+
+    return 0;
+}
